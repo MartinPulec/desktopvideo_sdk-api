2ca814109ff906a34448799c4e80889157ff110f
Martin Pulec <martin.pulec@cesnet.cz>
2024-11-18 12:25:40 +0100

    v12.6

--- Win/include/DeckLinkAPIVersion.h
+++ Win/include/DeckLinkAPIVersion.h
@@ -43,8 +43,8 @@
 #ifndef __DeckLink_API_Version_h__
 #define __DeckLink_API_Version_h__
 
-#define BLACKMAGIC_DECKLINK_API_VERSION					0x0c050000
-#define BLACKMAGIC_DECKLINK_API_VERSION_STRING			"12.5"
+#define BLACKMAGIC_DECKLINK_API_VERSION					0x0c060000
+#define BLACKMAGIC_DECKLINK_API_VERSION_STRING			"12.6"
 
 #endif	// __DeckLink_API_Version_h__
 
